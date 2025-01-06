b1fd86ece9b1acafc8128f67f3819a06f34042ce
Francesco Ariis <fa-ml@ariis.it>
2025-01-06 10:47:29 +0100

    Reformat executable code
    Some sensible changes, including adding signatures where necessary,
    qualifying imports, breaking up long lines, adding licences.

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,56 +1,54 @@
--- (C)  vasylp https://github.com/vasylp/hgettext/blob/master/src/hgettext.hs
-
-import qualified Data.Text as T
-import qualified Data.Text.IO.Utf8 as Utf8
-import qualified Language.Haskell.Exts as H
+-- Originally copied from https://github.com/vasylp/hgettext/
+-- © 2009 Vasyl Pasternak, BSD-3-Clause.
 
 import Options
-
-import Data.Time
-
-import Data.Generics.Uniplate.Data
-
--- import Distribution.Simple.PreProcess.Unlit
-
-import Data.List
-import Data.Ord
-import Data.Function (on)
-
-import Data.Version (showVersion)
 import Paths_haskell_gettext (version)
 
-toTranslate :: [String] -> H.ParseResult (H.Module H.SrcSpanInfo) -> [(H.SrcSpanInfo, String)]
-toTranslate f (H.ParseOk z) = nub [ (loc, s) | H.App _ (H.Var _ (H.UnQual _ (H.Ident _ x))) (H.Lit _ (H.String loc s _)) <- universeBi z, x `elem` f]
-toTranslate _ _ = []
+import qualified Data.Generics.Uniplate.Data as G
+import qualified Data.Function as F
+import qualified Data.List as L
+import qualified Data.Ord as O
+import qualified Data.Text as T
+import qualified Data.Time as TM
+import qualified Data.Text.IO.Utf8 as Utf8
+import qualified Data.Version as V
+import qualified Language.Haskell.Exts as H
 
--- Create list of messages from a single file
-formatMessages :: String -> [(H.SrcSpanInfo, String)] -> String
-formatMessages path l = concat $ map potEntry $ nubBy ((==) `on` snd) $ sortBy (comparing snd) l
-    where potEntry (wl, s) = unlines [
-                             "#: " ++ showSrc wl,
-                             "msgid " ++ (showStringC s),
-                             "msgstr \"\"",
-                             ""
-                            ]
-          showSrc wl = path ++ ":" ++ show (H.srcSpanStartLine (H.srcInfoSpan wl)) ++ ":" ++ show (H.srcSpanStartColumn (H.srcInfoSpan wl))
 
--- Escape a string in a C-like fashion,
--- see https://www.ibm.com/docs/en/i/7.4?topic=literals-string
-showStringC :: String -> String
-showStringC s0 = '"' : concatMap showCharC s0 ++ "\""
+main :: IO ()
+main = do
+    opts <- parseOptions
+    process opts
+
+process :: Options -> IO ()
+process Options{printVersion = True} =
+        putStrLn $ "hgettext (from haskell-gettext), version " ++
+                     (V.showVersion version)
+process opts
+    | null (inputFiles opts) = do
+        putStrLn "hgettext: missing arguments"
+    | otherwise = do
+        t <- mapM read' (inputFiles opts)
+        pot <- formatPotFile $
+                 map (\(n,c) -> formatMessages n $
+                                  toTranslate (keywords opts) c) t
+        Utf8.writeFile (outputFile opts) (T.pack pot)
     where
-      showCharC '"' = "\\\""
-      showCharC '\\' = "\\\\"
-      showCharC '\n' = "\\n"
-      showCharC c = return c
+        read' :: FilePath ->
+                 IO (String, H.ParseResult (H.Module H.SrcSpanInfo))
+        read' "-" = getContents >>= \c -> return ("-", H.parseFileContents c)
+        read' f = H.parseFile f >>= \m -> return (f, m)
+
+-------------------------------------------------------------------------------
+-- Write
 
 
 formatPotFile :: [String] -> IO String
 formatPotFile ls = do
-    time <- getZonedTime
-    let timeStr = formatTime defaultTimeLocale "%F %R%z" time
+    time <- TM.getZonedTime
+    let timeStr = TM.formatTime TM.defaultTimeLocale "%F %R%z" time
     let header = formatPotHeader timeStr
-    return $ concat $ header: ls
+    return $ concat (header:ls)
   where
     formatPotHeader :: String -> String
     formatPotHeader timeStr =
@@ -70,26 +68,40 @@ formatPotFile ls = do
                 "\"Content-Transfer-Encoding: 8bit\\n\"",
                 ""]
 
-process :: Options -> IO ()
-process Options{printVersion = True} =
-        putStrLn $ "hgettext (from haskell-gettext), version " ++
-                     (showVersion version)
-process opts
-    | null (inputFiles opts) = do
-        putStrLn "hgettext: missing arguments"
-    | otherwise = do
-        t <- mapM read' (inputFiles opts)
-        pot <- formatPotFile $
-                 map (\(n,c) -> formatMessages n $
-                                  toTranslate (keywords opts) c) t
-        Utf8.writeFile (outputFile opts) (T.pack pot)
+-- Create list of messages from a single file.
+formatMessages :: String -> [(H.SrcSpanInfo, String)] -> String
+formatMessages path l =
+        let sorted = L.sortBy (O.comparing snd) l
+            nubbed = L.nubBy ((==) `F.on` snd) sorted
+        in concatMap potEntry nubbed
     where
-        read' :: FilePath ->
-                 IO (String, H.ParseResult (H.Module H.SrcSpanInfo))
-        read' "-" = getContents >>= \c -> return ("-", H.parseFileContents c)
-        read' f = H.parseFile f >>= \m -> return (f, m)
+        potEntry :: (H.SrcSpanInfo, String) -> String
+        potEntry (wl, s) = unlines
+                             ["#: " ++ showSrc wl,
+                              "msgid " ++ (showStringC s),
+                              "msgstr \"\"",
+                              ""]
 
-main :: IO ()
-main = do
-    opts <- parseOptions
-    process opts
+        showSrc :: H.SrcSpanInfo -> String
+        showSrc wl = path ++ ":" ++
+                     show (H.srcSpanStartLine (H.srcInfoSpan wl)) ++ ":" ++
+                     show (H.srcSpanStartColumn (H.srcInfoSpan wl))
+
+toTranslate :: [String] -> H.ParseResult (H.Module H.SrcSpanInfo) ->
+               [(H.SrcSpanInfo, String)]
+toTranslate f (H.ParseOk z) =
+        L.nub [(loc, s) |
+                H.App _ (H.Var _ (H.UnQual _ (H.Ident _ x)))
+                        (H.Lit _ (H.String loc s _)) <- G.universeBi z,
+                x `elem` f]
+toTranslate _ _ = []
+
+-- Escape a string in a C-like fashion,
+-- see https://www.ibm.com/docs/en/i/7.4?topic=literals-string
+showStringC :: String -> String
+showStringC s0 = '"' : concatMap showCharC s0 ++ "\""
+    where
+      showCharC '"' = "\\\""
+      showCharC '\\' = "\\\\"
+      showCharC '\n' = "\\n"
+      showCharC c = return c
